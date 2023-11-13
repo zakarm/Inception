cbb3724de3c91dd8279f5e0ec21184db707afef3
zakarm <zakariaemrabet1@gmail.com>
2023-11-13 05:18:24 +0100

    fix wordpress config

--- srcs/requirements/wordpress/tools/script.py
+++ srcs/requirements/wordpress/tools/script.py
@@ -1,8 +1,23 @@
-import os, sys, time, subprocess
+import os, sys, time, subprocess, re
+
 try:
-    time.sleep(500)
-    subprocess.run(f"wp config create --dbname={os.environ['MYSQL_DATABASE_NAME']} --dbuser={os.environ['MYSQL_USER']} --dbpass={os.environ['MYSQL_PASSWORD']} --dbhost=mariadb --path=/var/www/wordpress --allow-root", shell=True, check=True)
-    subprocess.run(f"wp core install --url={os.environ['WP_URL']} --title={os.environ['WP_TITLE']} --admin_user={os.environ['WP_ADMIN_USER']} --admin_password={os.environ['WP_ADMIN_PASSWORD']} --path=/var/www/wordpress --allow-root", shell=True, check=True)
-    subprocess.run(f"/usr/sbin/php-fpm7.3 -F", shell=True, check=True)
+    file = "/etc/php/7.4/fpm/pool.d/www.conf"
+    with open(file, "r") as f: 
+        data = f.read()
+    data = re.sub(r'listen = /run/php/php7.4-fpm.sock', "listen = 0.0.0.0:9000", data)
+    with open(file, "w") as f: 
+        f.write(data)
+    time.sleep(5)
+    file = "/var/www/html/wp-config.php"
+    with open(file, "r") as f: 
+        data = f.read()
+    data = re.sub(r'database_name_here', os.environ['MYSQL_DATABASE_NAME'], data)
+    data = re.sub(r'username_here', os.environ['MYSQL_USER'], data)
+    data = re.sub(r'password_here', os.environ['MYSQL_PASSWORD'], data)
+    with open(file, "w") as f: 
+        f.write(data)
+    # subprocess.run(f"wp config create --dbname={os.environ['MYSQL_DATABASE_NAME']} --dbuser={os.environ['MYSQL_USER']} --dbpass={os.environ['MYSQL_PASSWORD']} --dbhost=mariadb --path=/var/www/html --allow-root", shell=True, check=True)
+    subprocess.run(f"wp core install --url={os.environ['WP_URL']} --title={os.environ['WP_TITLE']} --admin_user={os.environ['WP_ADMIN_USER']} --admin_password={os.environ['WP_ADMIN_PASSWORD']} --admin_email={os.environ['WP_ADMIN_EMAIL']} --path=/var/www/html --allow-root", shell=True, check=True)
+    subprocess.run(f"/usr/sbin/php-fpm7.4 -F", shell=True, check=True)
 except KeyboardInterrupt:
     sys.exit(0)
