0281dc96eca8a8c99674611e58af820f0d778df4
Zakariae Mrabet <zmrabet@e3r10p12.1337.ma>
2023-12-10 17:03:35 +0100

    add service of my choice

--- srcs/requirements/nginx/tools/script.py
+++ srcs/requirements/nginx/tools/script.py
@@ -5,3 +5,9 @@ with open(file, "r") as f:
 data = re.sub(r'/etc/nginx/ssl/',  os.environ["CERTS_"], data)
 with open(file, "w") as f:
     f.write(data)
+file = "/etc/nginx/sites-enabled/default"
+with open(file, "r") as f:
+    data = f.read()
+data = re.sub(r'localhost',  os.environ["DOMAIN_NAME"], data)
+with open(file, "w") as f:
+    f.write(data)
