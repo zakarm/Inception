dc7e8e0d7b0e2a141e9835f91e23441b293d73b8
Zakariae Mrabet <zmrabet@e2r5p12.1337.ma>
2024-01-12 18:47:55 +0100

    fix mariadb conf

--- srcs/requirements/mariadb/tools/script.py
+++ srcs/requirements/mariadb/tools/script.py
@@ -1,18 +1,21 @@
-import subprocess, os, sys, time
-try:
-    subprocess.run("service mariadb start", shell=True, check=True)
-    time.sleep(1)
-    sql_script = f"""
+from subprocess import run
+import os, time
+
+def run_c(cmd, res):
+    if res: run(cmd, check=True, shell=True)
+    else: return run(cmd, check=True, shell=True, capture_output=True, text=True)
+
+run_c("service mariadb start", False)
+while run_c(f"mysqladmin ping -u root -p{os.environ['MYSQL_ROOT_PASSWORD']}", True).stdout != "mysqld is alive\n": time.sleep(0.5)
+sql_script = f"""
     CREATE DATABASE IF NOT EXISTS {os.environ["MYSQL_DATABASE_NAME"]};
     CREATE USER IF NOT EXISTS '{os.environ["MYSQL_USER"]}'@'%' IDENTIFIED BY '{os.environ["MYSQL_PASSWORD"]}';
     GRANT ALL PRIVILEGES ON {os.environ["MYSQL_DATABASE_NAME"]}.* TO '{os.environ["MYSQL_USER"]}'@'%';
     ALTER USER 'root'@'localhost' IDENTIFIED BY '{os.environ["MYSQL_ROOT_PASSWORD"]}';
     FLUSH PRIVILEGES;
-    """
-    with open("db1.sql", "w") as f: f.write(sql_script)
-    subprocess.run(f"mysql -u root -p'{os.environ['MYSQL_ROOT_PASSWORD']}' < db1.sql", shell=True, check=True)
-    subprocess.run("rm -rf db1.sql", shell=True, check=True)
-    subprocess.run(f"mysqladmin -u root -p{os.environ['MYSQL_ROOT_PASSWORD']} shutdown", shell=True, check=True)
-    subprocess.run(f"mysqld", shell=True, check=True)
-except KeyboardInterrupt:
-    sys.exit(0)
+"""
+with open("db1.sql", "w") as f: f.write(sql_script)
+run_c(f"mysql -u root -p'{os.environ['MYSQL_ROOT_PASSWORD']}' < db1.sql", False)
+run_c("rm -rf db1.sql", False)
+run_c(f"mysqladmin -u root -p{os.environ['MYSQL_ROOT_PASSWORD']} shutdown",False)
+run_c(f"mysqld", False)
--- srcs/requirements/nginx/tools/script.py
+++ srcs/requirements/nginx/tools/script.py
@@ -2,7 +2,7 @@ import os, re
 file = "/etc/nginx/sites-enabled/default"
 with open(file, "r") as f:
     data = f.read()
-data = re.sub(r'/etc/nginx/ssl/',  os.environ["CERTS_"], data)
+data = re.sub(r'/etc/nginx/ssl/',  os.environ["CERTS_"], data) 
 with open(file, "w") as f:
     f.write(data)
 data = ""
