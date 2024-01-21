caff8f650fbbc75af948809125475fe0b759bdb1
Zakariae Mrabet <zmrabet@e3r10p12.1337.ma>
2024-01-21 02:14:08 +0100

    reverse proxy adminer

--- srcs/requirements/bonus/adminer/tools/script.py
+++ srcs/requirements/bonus/adminer/tools/script.py
@@ -1,2 +1,2 @@
 from os import system
-system("cd /var/www/html && rm -rf index.html && php -S 0.0.0.0:80")
+system("cd /var/www/html && rm -rf index.html && php -S 0.0.0.0:80 -t /var/www/html /var/www/html/adminer.php")
